2473630d00243a95be6f4de0038550263edecf3f
Ashwini Kumar Nayak <88447237+anayak2206@users.noreply.github.com>
2023-09-30 12:36:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+require('dotenv').config()
 
 
 app.get('/',(req,res)=>{
     res.send("Hello world")
 })
-app.listen(port,()=>{
+app.listen(process.env.PORT,()=>{
     console.log(`The app is listening to port ${port}`)
 })
 
